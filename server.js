81afa05833f2dc7476becd7c1471e0fd4afd7f34
Samira Tahirou <tahirousamira@gmail.com>
2023-01-16 02:14:39 +0100

    Sign up

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-console.log("Hello world!");
-console.log(" ça va?");
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = 3000
+
+// Nos Midllewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+
+app.post("/api/auth/signup", (req,res) => {
+    console.log("signup request:", req.body)
+    res.send({ message: "Inscription effectuée !"})
+})
+app.get('/',(req, res) => res.send("Hello, world!"))
+app.listen(port, () => { console.log("listening on port" + port)}) // Ecoute du port
