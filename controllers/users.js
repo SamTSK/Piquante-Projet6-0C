235d05795bb71efaacd50844b4930b6e509309c1
Samira Tahirou <tahirousamira@gmail.com>
2023-01-17 01:19:03 +0100

    Hashing - env

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,24 @@
-const {User} = require("../mongodb")
+const { User } = require("../mongodb")
+const bcrypt = require('bcrypt');
 
 
-function createUser(req, res) {
+async function createUser(req, res) {
     // recupérer mon name et mon password par la requête 
-    const email = req.body.email
-    const password = req.body.password
+    const { email, password } = req.body
+    
+    const hashedPassword = await hashPassword(password)
+
     // modèle schéma
-    const user = new User({email: email, password: password})
-user
+    const user = new User({ email: email, password: hashedPassword })
+    user
     .save()
     .then(() => res.send({ message: "Registration completed"}))
-    .catch(err => console.log("Registration failed !", err))
+    .catch((err) => console.log("Registration failed !", err))
+}
+
+function hashPassword(password) {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds,)
 }
 
 module.exports = {createUser}
--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,8 @@
 // Database
 const mongoose = require('mongoose')
-const password = "Oc2023!"
-const uri = `mongodb+srv://user1:${password}@cluster0.krf0fda.mongodb.net/?retryWrites=true&w=majority`;
+const password = process.env.DB_PASSWORD
+const username = process.env.DB_USER
+const uri = `mongodb+srv://${username}:${password}@cluster0.krf0fda.mongodb.net/?retryWrites=true&w=majority`;
 
 
 mongoose
