fe651d95d8782da26fe338758ee4fa88a143010e
Samira Tahirou <tahirousamira@gmail.com>
2023-01-18 23:05:22 +0100

    JWT json web token

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const { User } = require("../mongodb")
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function createUser(req, res) {
     // recupérer mon name et mon password par la requête 
@@ -21,18 +21,30 @@ function hashPassword(password) {
     return bcrypt.hash(password, saltRounds,)
 }
 async function logUser(req, res) {
-    const email = req.body.email
-    const password = req.body.password
-    const user = await User.findOne({email: email})
+        try { 
 
-    const isPasswordOk = await bcrypt.compare(password, user.password)
-    if (!isPasswordOk) {
-        res.status(403).send({ message : " Incorrect password"})
-    }
-    if (isPasswordOk) {
-        res.status(200).send({ message : " Successfull Connexion"})
-    }
+        const email = req.body.email
+        const password = req.body.password
+        const user = await User.findOne({email: email})
+        
+        const isPasswordOk = await bcrypt.compare(password, user.password)
+        if (!isPasswordOk) {
+            res.status(403).send({ message : " Incorrect password"})
+        }
+        const token = createToken(email)
+        res.status(200).send({ userId: user._id, token: token})
+        } catch (err) {
+            console.error(err)
+            res.status(500).send({ message: "Erreur interne"})
+        }
 
+    }  
+
+function createToken(email) {
+    const jwtPassword = process.env.JWT_PASSWORD
+    const token = jwt.sign({email: email}, jwtPassword, {expiresIn: "24h"})
+    return token
 }
 
+//User.deleteMany({}).then(()=> console.log("all removed"))  // remove all users
 module.exports = {createUser, logUser}
